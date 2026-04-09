be97cf49d733b371bb4bd2c64b2f1733fec6f7bb
tufailbaba <tufailbabacoc@gmail.com>
2026-04-09 12:30:06 +0530

    Callbacks

--- validation/app/models/user.rb
+++ validation/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
  validates :name, presence: true
  validates :email, presence: true, uniqueness: true
  validates :age, presence: true, length: { minimum: 1, maximum: 2 }
+ after_commit :testCallback
+  
+  private
+
+  def testCallback
+    puts "Hey im called"
+  end
 end
