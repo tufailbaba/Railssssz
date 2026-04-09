70262932e41a8447f105330e2e28423a9e31e7d5
tufailbaba <tufailbabacoc@gmail.com>
2026-04-09 12:35:56 +0530

    Dirty Methods

--- validation/app/models/user.rb
+++ validation/app/models/user.rb
@@ -2,11 +2,15 @@ class User < ApplicationRecord
  validates :name, presence: true
  validates :email, presence: true, uniqueness: true
  validates :age, presence: true, length: { minimum: 1, maximum: 2 }
- after_commit :testCallback
+#  after_commit :testCallback if saved_change_to_email?
   
   private
 
   def testCallback
-    puts "Hey im called"
+    puts "Hello Callback"
   end
+  # def testMethod
+  #   binding.pry
+  #   puts "Hello Dirty Method"
+  # end
 end
